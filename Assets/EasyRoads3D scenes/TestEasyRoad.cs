80a91b74f4b7c7e82aa28b20d3076ed9f33c4d71
TastSong <tastsong@163.com>
2021-03-27 22:42:38 +0800

    test easy roads

---
+++ Assets/EasyRoads3D scenes/TestEasyRoad.cs
@@ -0,0 +1,21 @@
+﻿using EasyRoads3Dv3;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEasyRoad : MonoBehaviour
+{
+    public ERRoad[] markers;
+    public ERRoadNetwork roadNetwork;
+
+    private void Start() {
+        BuildAllRoute();
+    }
+    private void BuildAllRoute() {
+        markers = roadNetwork.GetRoads();
+        for (int i = 0; i < markers.Length; i++) {
+            Debug.Log( i + "  GetMarkerPositions  = " + markers[i].GetMarkerPositions());
+            Debug.Log( i + "  GetSplinePointsCenter  = " + markers[i].GetSplinePointsCenter());
+        }
+    }
+}
