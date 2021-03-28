043c7d9e2bcce7782880a652868fc4e6fa371e12
TastSong <tastsong@163.com>
2021-03-28 22:11:35 +0800

    add Player

---
+++ Assets/AIBikeRobot/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance = null;
+
+    private void Awake() {    //防止存在多个单例
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/AIBikeRobot/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyRoads3Dv3;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public ERModularBase modularBase;
+
+    private Rigidbody rigidbody;
+
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        //控制移动
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rigidbody.MovePosition(transform.position + new Vector3(-h, 0, -v * 1.6f) * speed * Time.deltaTime);
+        rigidbody.MoveRotation(Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, h * 2.4f, 0) * speed * Time.deltaTime));
+    }
+        
+}
